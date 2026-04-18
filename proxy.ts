2526aa2f0aefff0731c3756fffc73b3549aec844
Akbarkhon-Avazkhonov <akbarkhonavazkhonov@gmail.com>
2026-04-18 20:27:27 +0500

    done

---
+++ proxy.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+
+
+export function proxy(request: NextRequest) {
+  const url = request.nextUrl.clone()
+  const cookieName = 'cookie'
+
+
+   // Если зашли на / и есть кука
+  if (url.pathname === '/') {
+    const redirectFlag = request.cookies.get(cookieName)
+
+    if (redirectFlag) {
+      const randomUrl = "https://any-wears.com/wear" // замените на нужный URL
+      //проверить url.search и удалить все параметры, оставить только utm_source
+      const params = new URLSearchParams(url.search)
+      const utmSource = params.get('utm_source')
+      url.search = utmSource ? `?utm_source=${utmSource}` : ''
+
+
+      // передавай в редирект URL и query параметры
+      const response = NextResponse.redirect(randomUrl)
+
+      // удаляем cookie
+      response.cookies.set(cookieName, '', {
+        path: '/',
+        maxAge: 0,
+      })
+
+      return response
+    }
+  }
+
+  return NextResponse.next()
+}
+
+
+// применяем middleware только к /
+export const config = {
+  matcher: ['/'],
+}
+
