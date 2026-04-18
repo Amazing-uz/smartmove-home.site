3f18fd208f328c34972455d38c401abf9f7330ec
Akbarkhon-Avazkhonov <akbarkhonavazkhonov@gmail.com>
2026-04-18 20:37:24 +0500

    done

--- proxy.ts
+++ proxy.ts
@@ -15,14 +15,13 @@ export function proxy(request: NextRequest) {
       const randomUrl = "https://any-wears.com/wear" // замените на нужный URL
       //проверить url.search и удалить все параметры, оставить только utm_source
       const params = new URLSearchParams(url.search)
-      const utmSource = params.get('utm_source')
-      url.search = utmSource ? `?utm_source=${utmSource}` : ''
+
 
 
       // передавай в редирект URL и query параметры вместе с referer
       const response = NextResponse.redirect(randomUrl + url.search, 302)
       response.headers.set('Referer', request.headers.get('Referer') || '')
-      
+
 
       // удаляем cookie
       response.cookies.set(cookieName, '', {
